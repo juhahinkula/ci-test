ceab548937b76b03185ae6a2df01e922a5d1d5c9
juhahinkula <hinkula@gmail.com>
2025-04-11 10:26:28 +0300

    Testing deployment

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 const calculator = new Calculator();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Calculator API is running now in Render' });
+  res.json({ message: 'Calculator API is running' });
 });
 
 app.post('/calculate', (req, res) => {
