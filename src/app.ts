85b04b7d17b5e84a474fa8be16d824e33f2f5ecd
juhahinkula <hinkula@gmail.com>
2025-04-11 13:15:29 +0300

    Render deployment workflow

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 const calculator = new Calculator();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Calculator API is running on Render.com' });
+  res.json({ message: 'Calculator API is running' });
 });
 
 app.post('/calculate', (req, res) => {
