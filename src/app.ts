34503682813dd5bc8e9bcd64a77644f5cc748078
juhahinkula <hinkula@gmail.com>
2025-04-22 09:26:37 +0300

    workflow modified

--- src/app.ts
+++ src/app.ts
@@ -43,7 +43,7 @@ const PORT = process.env.PORT || 3000;
 
 if (require.main === module) {
   app.listen(PORT, () => {
-    console.log(`Test Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
